a56fc1cc20a01deb4765916658fa137b8b06ca40
Jose A. Ricardo <ricardoingenierias@gmail.com>
2017-09-27 21:56:19 -0500

    configurando firebase y crando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDO_PG1JAqXqnK10E7Znft-S7oHLoIAUK0',
+  authDomain: 'personal-page-61d5b.firebaseapp.com',
+  databaseURL: 'https://personal-page-61d5b.firebaseio.com',
+  storageBucket: 'personal-page-61d5b.appspot.com',
+  messagingSenderId: '403988672319'
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
